ee41b1fc78eff6ec2861880299409d439a5852be
siva.anveti@gmail.com <siva anveti>
2019-01-13 23:15:11 +0530

    Firebase Installed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2S_xeRxrk836ugjdSclXZSJfZvQDA98Y",
+    authDomain: "ng-wiki.firebaseapp.com",
+    databaseURL: "https://ng-wiki.firebaseio.com",
+    projectId: "ng-wiki",
+    storageBucket: "ng-wiki.appspot.com",
+    messagingSenderId: "203959945733"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC2S_xeRxrk836ugjdSclXZSJfZvQDA98Y",
+    authDomain: "ng-wiki.firebaseapp.com",
+    databaseURL: "https://ng-wiki.firebaseio.com",
+    projectId: "ng-wiki",
+    storageBucket: "ng-wiki.appspot.com",
+    messagingSenderId: "203959945733"
+  }
 };
 
 /*
