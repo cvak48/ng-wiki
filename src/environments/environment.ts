9271b62f2637f1b120029883368ef4c11ffaf8c2
siva.anveti@gmail.com <siva anveti>
2019-01-14 00:22:32 +0530

    Bootstarp 4 Installed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC2S_xeRxrk836ugjdSclXZSJfZvQDA98Y",
-    authDomain: "ng-wiki.firebaseapp.com",
-    databaseURL: "https://ng-wiki.firebaseio.com",
-    projectId: "ng-wiki",
-    storageBucket: "ng-wiki.appspot.com",
-    messagingSenderId: "203959945733"
+    apiKey: 'AIzaSyC2S_xeRxrk836ugjdSclXZSJfZvQDA98Y',
+    authDomain: 'ng-wiki.firebaseapp.com',
+    databaseURL: 'https://ng-wiki.firebaseio.com',
+    projectId: 'ng-wiki',
+    storageBucket: 'ng-wiki.appspot.com',
+    messagingSenderId: '203959945733'
   }
 };
 
