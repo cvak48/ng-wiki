ff49d6eaf7ab40890921cfcd1c16987cc9bcbd9e
siva.anveti@gmail.com <siva anveti>
2019-01-14 09:46:03 +0530

    Ng-Bootstrap Installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,52 @@
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import { AdminProjectsComponent } from './admin/admin-projects/admin-projects.component';
+import { AdminPostsComponent } from './admin/admin-posts/admin-posts.component';
+import { LoginComponent } from './login/login.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    MyPostsComponent,
+    AdminProjectsComponent,
+    AdminPostsComponent,
+    LoginComponent,
+    BsNavbarComponent,
+    PagenotfoundComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'logout', component: LogoutComponent},
+      {path: 'posts', component: MyPostsComponent},
+      {path: 'admin/admin-projects', component: AdminProjectsComponent},
+      {path: 'admin/admin-posts', component: AdminPostsComponent},
+      { path: '**', component: PagenotfoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
