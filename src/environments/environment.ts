844eb903a30f262080df2bcccfcc485aa12d19e3
siva.anveti@gmail.com <siva anveti>
2019-01-14 12:30:20 +0530

    Deployed into Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyC2S_xeRxrk836ugjdSclXZSJfZvQDA98Y',
-    authDomain: 'ng-wiki.firebaseapp.com',
-    databaseURL: 'https://ng-wiki.firebaseio.com',
-    projectId: 'ng-wiki',
-    storageBucket: 'ng-wiki.appspot.com',
-    messagingSenderId: '203959945733'
+    apiKey: 'AIzaSyAEjIWbq3MzuC6L-gFxrFI9oD1a24RsS9A',
+    authDomain: 'ng-wiki-5e477.firebaseapp.com',
+    databaseURL: 'https://ng-wiki-5e477.firebaseio.com',
+    projectId: 'ng-wiki-5e477',
+    storageBucket: 'ng-wiki-5e477.appspot.com',
+    messagingSenderId: '1081531413614'
   }
 };
 
